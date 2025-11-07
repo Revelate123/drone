2c077ebc8aa01e4a695c46291b22dd62468b858f
Tom <thomasduffett4@gmail.com>
2025-11-07 23:46:44 +1100

    Added code to load a map from program memory

--- main.asm
+++ main.asm
@@ -39,6 +39,27 @@ explored_map_start: .byte 15*15
 route_size: .byte 1
 route_locations: .byte 15*15*3 ; Save x, y, z for each location (worst case we look at every location, but very unlikely to happen)
 
+.cseg
+.org 0x1000
+main:
+; Map from project brief
+map_size: .db 7*7
+map: .db 0,0,0,0,0,0,0 , 0,2,2,2,2,2,0 , 0,2,4,4,4,2,0 , 0,2,4,6,4,2,0 , 0,2,4,4,4,2,0 ,  0,2,2,2,2,2,0 , 0,0,0,0,0,0,0
+
+ldi ZL , low(map_size<<1)
+ldi ZH, high(map_size<<1)
+LPM r16, Z
+sts main_map_size, r16
+ldi ZL, low(map<<1)
+ldi ZH, high(map<<1)
+ldi YL, low(main_map_start)
+ldi YH, high(main_map_start)
+store_map_loop:
+	LPM r17, Z+
+	st Y+, r17
+	dec r16
+	brne store_map_loop
+
 
 
 
